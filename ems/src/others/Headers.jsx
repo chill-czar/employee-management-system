fd14097507d508ff6b57be4e77a75f872d6dee38
aisarthak <aisarthak2@gmail.com>
2024-11-05 17:02:03 +0530

    headers done logout function sync with backend data
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- ems/src/others/Headers.jsx
+++ ems/src/others/Headers.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-const Headers = () => {
+const Headers = ({data}) => {
+  console.log(data)
+  // const {name} = data
+
+ function Logout() {
+    localStorage.removeItem('loggedInUser')
+    window.location.reload()
+  }
+
+  // Example usage // Replace 'myKey' with the actual key you want to delete
+
   return (
     <div className="flex items-end justify-between">
-          <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl'> Sarthak 👋</span></h1>
-        <button className='bg-red-500 text-white py-2 px-5 rounded-sm text-lg font-medium'>Logout</button>
+          <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl'> {data.data.name} 👋</span></h1>
+        <button className='bg-red-500 text-white py-2 px-5 rounded-sm text-lg font-medium' onClick={Logout}>Logout</button>
     </div>
   )
 }
