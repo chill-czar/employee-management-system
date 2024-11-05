3c6f111e3df54e377c489a4d2d853ad79c90a094
aisarthak <aisarthak2@gmail.com>
2024-11-05 17:44:40 +0530

    task list done self done
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const TaskList = () => {
+const TaskList = ({ data }) => {
+    const cardData = data.data.tasks; // array
+    console.log(cardData);
+
     return (
-        <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 px-5  mt-10 '>
-            <div className=' flex-shrink-0 h-full w-[300px]  bg-red-400 rounded-xl p-5 '>
-                <div className='flex justify-between items-center '> 
-
-                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                    <h4 className='text-sm'>20 feb 2024</h4>
-                </div>
-                <h2 className='mt-5 text-2xl font-semibold'>Make a youtebe video</h2>
-                <p className='text-sm mt-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus placeat sint aliquam ad cumque maiores?</p>
+        <>
+            <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 px-5 mt-10'>
+                {cardData.map((element, index) => (
+                    <div key={index} className='flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl p-5'>
+                        <div className='flex justify-between items-center'>
+                            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> {element.category} </h3>
+                            <h4 className='text-sm'> {element.taskDate} </h4>
+                        </div>
+                        <h2 className='mt-5 text-2xl font-semibold'>{element.taskTitle}</h2>
+                        <p className='text-sm mt-2'> {element.taskDescription} </p>
+                    </div>
+                ))}
             </div>
+        </>
+    );
+};
 
-        </div>
-    )
-}
-
-export default TaskList
+export default TaskList;
