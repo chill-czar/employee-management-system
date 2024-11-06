364186d75803f8cd618668eefc2d0dcff71273ea
aisarthak <aisarthak2@gmail.com>
2024-11-06 05:54:02 +0530

    headers working with admin and employee
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- ems/src/others/Headers.jsx
+++ ems/src/others/Headers.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 
-const Headers = ({data}) => {
-  console.log(data)
+const Headers = ({ data }) => {
+  console.log(data.name)
   // const {name} = data
 
- function Logout() {
+  function Logout() {
     localStorage.removeItem('loggedInUser')
     window.location.reload()
   }
 
+  let name = ''
+  if (localStorage.getItem('loggedInUser') == '{"role":"admin"}') {
+    name = data.name
+  } else {
+    name = data.data.name
+  }
   // Example usage // Replace 'myKey' with the actual key you want to delete
 
   return (
     <div className="flex items-end justify-between">
-          <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl'> {data.data.name} 👋</span></h1>
-        <button className='bg-red-500 text-white py-2 px-5 rounded-sm text-lg font-medium' onClick={Logout}>Logout</button>
+      <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl'>
+        {/* {data.data.name}  */}
+        {name}
+        👋</span></h1>
+      <button className='bg-red-500 text-white py-2 px-5 rounded-sm text-lg font-medium' onClick={Logout}>Logout</button>
     </div>
   )
 }
